cb065530a0ee27d6026bd7c44f419ffeeaf2ec82
jmb463 <32370337+jmb463@users.noreply.github.com>
2018-02-08 21:00:51 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,5 +1,5 @@
-// fichoro xml que está en el servidor rawgit
-var url="https://cdn.rawgit.com/jmb463/ExamenHTML-CSS/5c45750a/Preguntas.XML";
+// fichero xml que está en el servidor rawgit
+var url="https://cdn.rawgit.com/jmb463/ExamenHTML-CSS/aaf22c90/xml/Preguntas.XML";
 
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
@@ -19,4 +19,19 @@ function gestionarXml(dadesXml){
     var title= xmlDoc.getElementsByTagName("title")[i].innerHTML;
     document.getElementsByTagName("h3")[i].innerHTML = title;
   }
-}
+
+  for (i = 2; i < 6; i++) {
+    var opcionesSelect = [];
+    var nopt = xmlDoc.getElementsByTagName("question")[i].getElementsByTagName('option').length;
+    for (j = 0; j < nopt; j++) {
+        opcionesSelect[j] = xmlDoc.getElementsByTagName("question")[i].getElementsByTagName('option')[j].innerHTML;
+    }
+
+	var select = document.getElementsByTagName("select")[i - 2];
+	for (k = 0; k < opcionesSelect.length; k++) {
+    	var option = document.createElement("option");
+    	option.text = opcionesSelect[k];
+    	option.value = k + 1;
+    	select.options.add(option);
+    }
+  }
